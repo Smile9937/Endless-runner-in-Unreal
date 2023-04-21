8e4bc364e2dcd84fde34bc7888946409df174911
Simon Persson <smile9937@gmail.com>
2023-04-21 09:19:21 +0200

    Various files

--- Source/EndlessRunner/EndlessRunner.Build.cs
+++ Source/EndlessRunner/EndlessRunner.Build.cs
@@ -9,5 +9,6 @@ public class EndlessRunner : ModuleRules
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] { "EndlessRunner" });
 	}
 }
